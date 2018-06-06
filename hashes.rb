af8579fd6f090f13bf2aacfd1d504e08273d3604
bayleighanne <bayhaf718@gmail.com>
2018-06-06 23:09:57 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,37 @@
+# #A HASH IS MADE UP OF KEY/VALUE PAIRS 
+# #hash = {"cheese" => "the stuff on pizza"}
+
+# characters_names = ["Monica", "Joey", "Phoebe"] #key 
+# characteristics = ["Neat Freak", "Jokester", "Free Spirit"] #values
+
+# friends_characters = {}
+
+# index = 0
+# character_names.each do |name|
+#   friends_characters[name] = characteristics[index]
+#   index += 1
+# end
+# puts friends_characters
+
+# celeb_crush["Emi"] = "Grant Gustin"
+# puts celeb_crush 
+
+# class_celeb_crushes = {
+#   "Natalia" => "Timothee Chalemet", 
+#   "Maya" => "Michael B. Jordan",
+#   "Amira" => "Cole Sprouse",
+# }
+# puts class_celeb_crushes 
+# # puts class_celeb_crushes.values
+# # puts class_celeb_crushes.keys 
+
+# class_celeb_crushes.each do |names,crushes|
+#   puts "#{names} crush is #{crushes}"
+# end 
+
+puts "3" + 3.0
+
+# .to_f -Float
+# .to_i - Flixnum or Integer
+# .to_s - Symbol
+# .to_str - String 
